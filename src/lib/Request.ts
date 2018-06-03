6acbeba7f2c0e59655a80b14361b3b1a110fc8f7
The Jewish Rabbi <jewishrabbi@gmail.com>
2018-06-03 11:46:33 +0000

    Major fix for request module.

--- src/lib/Request.ts
+++ src/lib/Request.ts
@@ -7,14 +7,19 @@ const log = Logger();
 
 class Request {
   public scrape(url: string, callback: any, control: any, ...data: any[]){
-    request('GET',url).done((res)=>{
-      if( res.statusCode == 200 ) {
-        callback(url, cheerio.load(String(res.getBody())), ...data);
-      }
-      else{
-        log.error(`Cannot scrape url: ${url}, status code: ${res.statusCode}`);
-      }
-    });
+    try {
+      request('GET', url).done((res) => {
+        if (res.statusCode == 200) {
+          callback(url, cheerio.load(String(res.getBody())), ...data);
+        }
+        else {
+          log.error(`Cannot scrape url: ${url}, status code: ${res.statusCode}`);
+        }
+      });
+    }
+    catch(e){
+      log.warn(e);
+    }
   }
 
 }
