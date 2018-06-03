3443d61907bf5a1b0949aae69b10ed4bf77b63e1
The Jewish Rabbi <jewishrabbi@gmail.com>
2018-06-03 09:37:28 +0000

    Fixes #24. Adding in new sensations. reverting to simple then-request.

--- src/lib/Request.ts
+++ src/lib/Request.ts
@@ -1,54 +1,20 @@
 import cheerio from 'cheerio';
 import Logger from './Logger';
-const phantomPool = require('phantom-pool');
+import request from 'then-request';
 const log = Logger();
 
-const max_pool = (process.env.MAXPOOL)? parseInt(process.env.MAXPOOL) : 10;
-const pool = phantomPool({
-  max: max_pool,
-  min: 1,
-  phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true'],{}]
-});
+
 
 class Request {
   public scrape(url: string, callback: any, control: any, ...data: any[]){
-    pool.use(async(instance: any)=>{
-      const page = await instance.createPage();
-      const status = await page.open(url);
-
-      if( status != 'success' ){
-        log.error('Could not scrape ' + url);
-        return;
+    request('GET',url).done((res)=>{
+      if( res.statusCode == 200 ) {
+        callback(url, cheerio.load(String(res.getBody())), ...data);
       }
-
-      page.property('onConsoleMessage',function(msg: string) {
-        console.log('CONSOLE: ' + msg);
-      });
-      await page.evaluate(function() {
-        if (typeof (<any>window).click !== "function") {
-          (<any>window).click = function (el: any) {
-            var ev = document.createEvent('MouseEvent');
-            ev.initMouseEvent(
-              'click',
-              true, true,
-              window, 0,
-              0, 0, 0, 0,
-              false, false, false, false,
-              0, null
-            );
-            el.dispatchEvent(ev);
-          }
-        }
-      });
-      await control(instance,page);
-
-      const content = await page.property('content');
-      callback(url, cheerio.load(content), ...data);
-
-      return content;
-    }).catch((error: any)=>{
-      log.warn( "Phantom browser issues:", error );
-    })
+      else{
+        log.error(`Cannot scrape url: ${url}, status code: ${res.statusCode}`);
+      }
+    });
   }
 
 }
---
+++ src/scrapers/newsensations.ts
@@ -0,0 +1,40 @@
+import Scraper from './Scraper.js';
+
+class newsensations extends Scraper {
+  protected site: string[] = ['newsensations'];
+  protected paysite: string[] = ['New Sensations'];
+  protected url: string[] = ["https://www.newsensations.com/tour_ns/category.php?id=5&page=%d&s=d"];
+  protected increment: number=1;
+
+  protected scrape_page(url: string, $:any, index: number, self: any):void {
+    let baseurl = 'https://newsensations.com/';
+    $('.update_details').each(function(i: number, elem: any){
+      let info = $(elem);
+      let scene = self.get_sceneObj(self.paysite[0]);
+      let scrape_url = baseurl + info.find('a').first().attr('href');
+      let date = info.find('.date_small').text().trim().replace(/Released:\s/,'');
+
+      scene.set_site( "New Sensations" );
+      scene.set_url( scrape_url );
+      //scene.set_image( info.find('.update_thumb').attr('src0_2x') );
+      scene.set_date(self.format_date( date,'MM/DD/YYYY' ));
+      scene.set_paysite_id(info.attr('data-setid'));
+
+      // grab scene description and actors and tags
+      self.get_page_content( scrape_url, function(url: string, $: any, scene:any){
+        scene.add_actors( $('.update_models').find('a').map(function(i: number, actor:any){
+          return $(actor).text();
+        }).get() );
+        scene.add_tags( $('.update_tags').find('a').map(function(i: number, actor:any){
+          return $(actor).text();
+        }).get() );
+        scene.set_description( $('.update_description').text().trim() );
+        scene.set_title( $('.update_title').text().trim() );
+
+        scene.commit();
+      },self.control,scene);
+    });
+  }
+
+}
+module.exports = newsensations;
